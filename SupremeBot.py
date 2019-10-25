75b23bbe95f2df22c32f6df74ed5b3274516f9e8
garrettleising <gzleising@gmail.com>
2019-10-24 18:10:12 -0700

    Can now choose which color you want

--- SupremeBot.py
+++ SupremeBot.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 import re
 import time
 from datetime import datetime
-from pytz import timezone    
+from pytz import timezone
 
 name = ""
 email = ""
@@ -25,8 +25,10 @@ cvv = ""
 itemName = ""
 
 size = ""
-itemType = ""
+itemColor = ""
+itemType = "" # example: pants
 
+linkToSupreme = "https://www.supremenewyork.com"
 linkToStore = "https://www.supremenewyork.com/shop/all/" + itemType
 
 chrome_path = r'C:/Users/Garre/Desktop/Selenium Bot/Chrome Driver/chromedriver.exe'
@@ -38,21 +40,23 @@ USPacific_time = datetime.now(pacific)
 
 print(USPacific_time.strftime('%H:%M:%S'))
 
-#waits for the correct time to run the bot
-while (USPacific_time.strftime('%H:%M:%S') != "07:59:00"):
+# waits for the correct time to run the bot
+while (USPacific_time.strftime('%H:%M:%S') != "07:59:25"):
     time.sleep(0.5)
     USPacific_time = datetime.now(pacific)
 
 print(USPacific_time.strftime('%H:%M:%S'))
 
+# Removes images from site
 options = webdriver.ChromeOptions()
 prefs = {'profile.default_content_setting_values': {'images': 2}}
 options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(chrome_path, chrome_options=options)
 
+# driver = webdriver.Chrome(chrome_path)
 driver.get(linkToStore)
 
-#consantly checks for the item and refreshes the page
+# consantly checks for the item and refreshes the page
 while loop:
     productNames = driver.find_elements_by_class_name("name-link")
 
@@ -64,20 +68,29 @@ while loop:
     time.sleep(1)
     driver.refresh()
 
-#tries to select a size option if there is one
+try:
+    colorButtons = driver.find_elements_by_tag_name("button")
+    for option in colorButtons:
+        print(option.get_attribute("data-style-name"))
+        if re.search(itemColor, option.get_attribute("data-style-name"), re.IGNORECASE):
+            driver.get(linkToSupreme + option.get_attribute("data-url"))
+            break
+except:
+    print("Color Option Unavailable")
+
+# tries to select a size option if there is one
 try:
     sizeOptions = driver.find_element_by_xpath("//select[@name='s'][@id='s']")
     all_sizeOptions = sizeOptions.find_elements_by_tag_name("option")
     for option in all_sizeOptions:
-        if (option.text == size):
+        if re.search(size, option.text, re.IGNORECASE):
             option.click()
             break
 except:
     print("No size Option")
 
 
-
-#Waits for submit and checkout button to appear and clicks them
+# Waits for submit and checkout button to appear and clicks them
 wait = WebDriverWait(driver, 30)
 submit = wait.until(EC.element_to_be_clickable(
     (By.XPATH, "//input[@type='submit'][@name='commit'][@value='add to cart']")))
@@ -134,7 +147,7 @@ for option in creditYear_options:
 vval.send_keys(cvv)
 iCheckHelper.click()
 
-#30 mins of sleeping
+# 30 mins of sleeping
 time.sleep(1800)
 assert "No results found." not in driver.page_source
 driver.close()
