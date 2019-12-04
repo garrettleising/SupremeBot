c446b8aecadd3567e57c106806183a7fb51d8521
Leo Ott <leo.ott.iv@gmail.com>
2019-12-04 05:01:05 -0600

    Wrapped everything in functions

--- Run.py
+++ Run.py
@@ -1,10 +1,8 @@
 from os import walk
 from os import path
 
-## ideas: make a list for the ordered dict so it can index in a parallel array.
-
-keyList = ["itemName", "size", "itemColor", "itemType", "name", "email", "telephone", "address", "aptUnitEtc", "zipcode", "city", "state", "country", "creditCardNumber", "experationMonth", "experationYear", "cvv"]
 profiles = []
+keyList = ["itemName", "size", "itemColor", "itemType", "name", "email", "telephone", "address", "aptUnitEtc", "zipcode", "city", "state", "country", "creditCardNumber", "experationMonth", "experationYear", "cvv"]
 
 def readFile(toRead):
     orderedProfile = {}
@@ -15,18 +13,29 @@ def readFile(toRead):
         i+=2
     return orderedProfile
 
-for root, dirs, files in walk(".\Profiles"):
-    #dirs is any folders
-    for name in files:
-        f = open(path.join(root,name), "r")
-        toRead = ''.join(f.readlines())
-        profiles.append(readFile(toRead))
+def readProfile():
+    for root, dirs, files in walk(".\Profiles"):
+        #dirs is any folders
+        for name in files:
+            f = open(path.join(root,name), "r")
+            toRead = ''.join(f.readlines())
+            profiles.append(readFile(toRead))
 
-for instance in profiles:
-    print(instance)
-    #open
-    #load supreme
-    #refreshen
-    #clicky
-    #clicky clicky filly profiley
-    #if failed go back baby
+def addProfile():
+    print("Not done yet!")
+
+def removeProfile():
+    print("Not done yet!")
+
+def run():
+    for instance in profiles:
+        print(instance)
+        #open
+        #load supreme
+        #refreshen
+        #clicky
+        #clicky clicky filly profiley
+        #if failed go back baby
+
+readProfile()
+#make some gui to go between add, read, delete, and run.
