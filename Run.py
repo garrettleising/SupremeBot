f863029975073fbb9e96a703320c9be919f6d2a7
Leo Ott <leo.ott.iv@gmail.com>
2019-12-04 21:55:26 -0600

    Added comments

--- Run.py
+++ Run.py
@@ -3,9 +3,11 @@ from SupremeBot import doIt
 from os import walk
 from os import path
 
+#keyList is the order for profile.txt files
 profiles = []
 keyList = ["itemName", "size", "itemColor", "itemType", "name", "email", "telephone", "address", "aptUnitEtc", "zipcode", "city", "state", "country", "creditCardNumber", "experationMonth", "experationYear", "cvv"]
 
+#readFile decomposes each .txt file into a dictionary using keyList
 def readFile(toRead):
     orderedProfile = {}
     splice = toRead.split('"')
@@ -16,6 +18,7 @@ def readFile(toRead):
         i+=2
     return orderedProfile
 
+#readProfile will fetch all in the .\Profiles folder.
 def readProfile():
     for root, dirs, files in walk(".\Profiles"):
         #dirs is any folders
@@ -24,21 +27,24 @@ def readProfile():
             toRead = ''.join(f.readlines())
             profiles.append(readFile(toRead))
 
+#addProfile will add a profile to the .\Profiles folder
 def addProfile():
     print("Not done yet!")
 
 def removeProfile():
     print("Not done yet!")
 
+#Strictly for debugging
 def printProfile():
     for instance in profiles:
         print(instance)
 
+#Temp holder for running SupremeBot.py
 def run():
     for instance in profiles:
         task = threading.Thread(target = doIt, args = (instance,))
         task.start()
 
+#Temp holder for running run() (should be done with gui clicks)
 readProfile()
 run()
-#make some gui to go between add, read, delete, and run.
